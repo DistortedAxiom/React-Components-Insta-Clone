b7ffb46f8ee2666fd8ef1e2aa70667b028c1f968
Anh Truong <aqtruong97@gmail.com>
2020-05-12 20:23:39 -0400

    finished MVP with likes

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -1,17 +1,35 @@
 // You do not need to do anything in this file
-import React from 'react';
+import React, { useState } from 'react';
+import Comment from './Comment';
 
-const CommentInput = props => {
+const CommentInput = () => {
+
+  const [comment, updateComment] = useState("");
+  const user = "Anh";
+
+  const obj = {
+        username: `${user}`,
+        text: `${comment}`,
+  }
+
+  const submitComment = (e) => (
+    e.preventDefault(),
+    console.log(obj),
+    <Comment comment={obj}/>,
+    updateComment("")
+  );
+
   return (
-    <form className="comment-form" onSubmit={props.submitComment}>
+    <form className="comment-form" onSubmit={submitComment}>
       <input
         type="text"
-        value={props.comment}
+        value={comment}
         placeholder="Add comment... "
-        onChange={props.changeComment}
+        onChange={(e) => updateComment(e.target.value)}
       />
     </form>
   );
+
 };
 
 export default CommentInput;
